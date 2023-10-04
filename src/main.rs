55c6948df51f4c58a07e98d1a83bd516b9777852
Saeed Dadkhah <saeed@zilliqa.com>
2023-10-04 23:32:37 +0330

    First endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
