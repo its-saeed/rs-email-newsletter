ca76a2c2d45ec27786bb19557f1f09a23ca4f12e
Saeed Dadkhah <saeed@zilliqa.com>
2023-10-06 08:50:55 +0330

    split lib bin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use std::net::TcpListener;
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use zero_2_prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
