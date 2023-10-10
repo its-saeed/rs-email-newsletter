749a15c429624d29e306441909d2e4efaedfe84e
Saeed Dadkhah <saeed@zilliqa.com>
2023-10-10 15:29:12 +0330

    Instrument the code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero_2_prod::{
@@ -8,13 +9,14 @@ use zero_2_prod::{
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Unable to connect to database");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Unable to connect to database");
     let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
     run(listener, connection_pool)?.await
 }
